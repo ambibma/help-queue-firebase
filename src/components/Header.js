b78a375064270dfeacf60670534a39edf3816c43
ambi hidalgo <ambio.pk@gmail.com>
2023-06-26 20:37:38 -0700

    Add navbar
    Co-authored-by: Dani Steely <DaniS91@users.noreply.github.com>
    Co-authored-by: Cameron Abel <cameronabel@users.noreply.github.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,40 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import styled from 'styled-components'
 
 function Header() {
   return (
     <React.Fragment>
-      <h1>Help Queue</h1>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        
-        </li>
-        <li>
-          <Link to="/sign-in"> Sign In</Link>
-        </li>
-      </ul>
+      <Navbar>
+        <Link to="/" style={titleStyle}>Help Queue</Link>
+        <Link to="/sign-in" style={linkStyle}> Sign In</Link>
+      </Navbar>
     </React.Fragment>
   );
 }
+
+const linkStyle = {
+  textDecoration: "none",
+  fontWeight: "600",
+  fontSize: "1.5rem",
+  padding: "0.5em",
+  paddingLeft: "1.5em",
+  paddingRight: "0em",
+  color: "black"
+}
+
+const titleStyle = {
+  ...linkStyle,
+  fontSize: "2.5em",
+  fontWeight: "700"
+}
+
+const Navbar = styled.section`
+  display: flex;
+  align-items: center;
+  background-color: #7FD1B9;
+  margin-bottom: 1.5em;
+  min-width: 600px;
+`;
 
 export default Header;
